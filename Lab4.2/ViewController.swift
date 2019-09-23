6249a371b95a0cd85436638f628a25c4be7b3ee5
t6hote00 <t6hote00@students.oamk.fi>
2019-09-23 13:08:13 +0300

    Initial Commit

---
+++ Lab4.2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lab4.2
+//
+//  Created by Tero Honkala on 23/09/2019.
+//  Copyright © 2019 Tero Honkala. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
